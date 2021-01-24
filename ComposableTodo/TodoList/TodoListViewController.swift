411319223a04ad53bb8274051726dc0c856af4cc
Sunny.K <sunny.k@hpcnt.com>
2021-01-24 19:16:02 +0900

    add TodoListViewController

--- ComposableTodo/TodoList/TodoListViewController.swift
+++ ComposableTodo/TodoList/TodoListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TodoListViewController.swift
 //  ComposableTodo
 //
 //  Created by Sunny.K on 2021/01/24.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TodoListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
