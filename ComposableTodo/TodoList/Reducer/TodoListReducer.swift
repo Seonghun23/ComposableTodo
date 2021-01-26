fb06a801ee575bae31b0214f4dc2ee93ae93e3e3
Sunny.K <sunny.k@hpcnt.com>
2021-01-27 00:30:13 +0900

    combine reducer

--- ComposableTodo/TodoList/Reducer/TodoListReducer.swift
+++ ComposableTodo/TodoList/Reducer/TodoListReducer.swift
@@ -7,33 +7,59 @@
 
 import ComposableArchitecture
 
-let todoListReducer = Reducer<TodoListState, TodoListAction, TodoListEnvironment> { state, action, environment in
-  switch action {
-  case .toggle(let index):
-    var list = state.todoList
-    list[index].isDone.toggle()
-    return environment.save(list)
-        .map(TodoListAction.reload)
 
-  case .initialize:
-      return environment.load()
-        .catchToEffect()
-        .map { result -> [Todo] in
-            switch result {
-            case .success(let todo):
-                return todo
-            case .failure(let error):
-                return []
-            }
-        }
+let updateTodoListReducer = Reducer<TodoListState, TodoListAction, TodoListEnvironment> { state, action, environment in
+    switch action {
+    case .toggle(let index):
+      var list = state.todoList
+      list[index].isDone.toggle()
+      return environment.save(list)
           .map(TodoListAction.reload)
 
-  case .reload(let list):
-    state.todoList = list
-    return .none
+    case .initialize:
+        return environment.load()
+          .catchToEffect()
+          .map { result -> [Todo] in
+              switch result {
+              case .success(let todo):
+                  return todo
+              case .failure(let error):
+                  return []
+              }
+          }
+            .map(TodoListAction.reload)
 
-  case .toggleAddTodoPresent:
-    state.isAddTodoPresented = !state.isAddTodoPresented
-    return .none
+    case .reload(let list):
+      state.todoList = list
+      return .none
+
+    case .toggleAddTodoPresent:
+      state.isAddTodoPresented = !state.isAddTodoPresented
+      return .none
+    }
   }
-}
+
+let printTodoListLogReducer = Reducer<TodoListState, TodoListAction, TodoListEnvironment> { state, action, environment in
+    switch action {
+    case .toggle(let index):
+        print("toggle at: \(index)")
+        return .none
+
+    case .initialize:
+        print("initialize")
+        return .none
+
+    case .reload(let list):
+        print("reload")
+        return .none
+
+    case .toggleAddTodoPresent:
+        print("toggleAddTodoPresent")
+        return .none
+    }
+  }
+
+let todoListReducer: Reducer<TodoListState, TodoListAction, TodoListEnvironment> = .combine([
+    updateTodoListReducer,
+    printTodoListLogReducer
+])
